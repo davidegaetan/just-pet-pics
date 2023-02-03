468e01a8d0f29c2aafe40b64da9aeae5d069ff6a
dgaetan <dgaetan@ocm.com>
2023-02-03 16:25:41 -0300

    making localhost https

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors")
+const fs = require('fs');
+const key = fs.readFileSync('./cert/CA/localhost/localhost.decrypted.key');
+const cert = fs.readFileSync('./cert/CA/localhost/localhost.crt');
 const app = express();
 const PORT = 8080;
 
@@ -12,4 +15,15 @@ app.use(express.json(), express.urlencoded({ extended: true }));
 const AllRoutes = require("./server/routes/pets.routes");
 AllRoutes(app);
 
-app.listen(PORT, () => console.log(`The server is listening on port ${PORT}`));
+// app.listen(PORT, () => console.log(`The server is listening on port ${PORT}`));
+
+app.get('/', (req, res, next) => {
+    res.status(200).send('Hello world!');
+});
+
+const https = require('https');
+const server = https.createServer({ key, cert }, app);
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on https://localhost:${PORT}`);
+});
