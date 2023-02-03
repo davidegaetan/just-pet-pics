5d862dce4c1a59f6f816e43111547eab4395d850
dgaetan <dgaetan@ocm.com>
2023-02-03 16:45:29 -0300

    back to original server.js

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const cors = require("cors")
-const fs = require('fs');
-const key = fs.readFileSync('./cert/CA/localhost/localhost.decrypted.key');
-const cert = fs.readFileSync('./cert/CA/localhost/localhost.crt');
 const app = express();
 const PORT = 8080;
 
@@ -16,14 +13,3 @@ const AllRoutes = require("./server/routes/pets.routes");
 AllRoutes(app);
 
 app.listen(PORT, () => console.log(`The server is listening on port ${PORT}`));
-
-app.get('/', (req, res, next) => {
-    res.status(200).send('Hello world!');
-});
-
-const https = require('https');
-const server = https.createServer({ key, cert }, app);
-const port = 3000;
-server.listen(port, () => {
-    console.log(`Server is listening on https://localhost:${port}`);
-});
